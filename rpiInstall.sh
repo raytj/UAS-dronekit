9aca5335431bd205410d68a73eda6490231a076a
jtray1 <jtray1@crimson.ua.edu>
2019-04-12 00:38:32 -0500

    fixed install script for python3.7

--- rpiInstall.sh
+++ rpiInstall.sh
@@ -14,16 +14,16 @@ rm Python-3.7.0.tar.xz
 sudo apt-get --purge remove build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 sudo apt-get autoremove -y
 sudo apt-get clean
-python3 -V
-sudo pip3 install opencv-python -y
+python3.7 -V
+sudo pip3.7 install opencv-python -y
 sudo apt-get install python3-dev python-wxgtk3.0 python3-pip python3-matplotlib python3-pygame python3-lxml python3-yaml libxml2-dev git -y
-pip3 install MAVProxy
+pip3.7 install MAVProxy
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 sudo adduser pi dialout
 cd ../..
 git clone https://github.com/raytj/dronekit-python.git
 cd dronekit-python
-pip install -r install_requires.txt -y
-python setup.py build
-python setup.py install
-pip install digi-xbee -y
+pip3.7 install -r install_requires.txt -y
+python3.7 setup.py build
+python3.7 setup.py install
+pip3.7 install digi-xbee -y
